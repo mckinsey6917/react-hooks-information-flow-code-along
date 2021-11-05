bb4c10701a1640b5a9a2e5f3f58cd3455f9ef031
mckinseyleckenby <leckenbym11@gmail.com>
2021-11-05 10:19:59 -0500

    commit tree 46106c4e22d17a75cf73e7ecfb949c5511e2b1cb

--- src/Parent.js
+++ src/Parent.js
@@ -4,12 +4,22 @@ import Child from "./Child";
 
 function Parent() {
   const randomColor = getRandomColor();
-  const [color, setColor] = useState(randomColor);
+  const [color, setColor] = useState(randomColor); //initial value of color state
+  const [childrenColor, setChildrenColor] = useState("#FFF");
+
+  function handleChangeColor(newChildColor) {
+    const newRandomColor = getRandomColor();
+    setColor(newRandomColor); //update color state to a new value
+    setChildrenColor(newChildColor); //update to ALSO take in an argument of newChildColor
+    //now that the function takes an argument we can create a new function in our child 
+    //component that invokes onChangeColor and passes in random color as the argument
+    //also update childs onClick calback to include? the newfunction
+  }
 
   return (
     <div className="parent" style={{ backgroundColor: color }}>
-      <Child />
-      <Child />
+      <Child color={childrenColor} onChangeColor={handleChangeColor} />
+      <Child color={childrenColor} onChangeColor={handleChangeColor} />
     </div>
   );
 }
